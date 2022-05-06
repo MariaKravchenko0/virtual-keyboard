a0ac72945360201960ae7eac31db70f78c78a8dd
MariaKravchenko0 <kravchenkomaria00@gmail.com>
2022-05-06 20:15:17 +0300

    feat: add language layouts

---
+++ js/layouts/en.js
@@ -0,0 +1,322 @@
+export default [
+  {
+    small: "`",
+    capital: "~",
+    code: "Backquote",
+  },
+  {
+    small: "1",
+    capital: "!",
+    code: "Digit1",
+  },
+  {
+    small: "2",
+    capital: "@",
+    code: "Digit2",
+  },
+  {
+    small: "3",
+    capital: "#",
+    code: "Digit3",
+  },
+  {
+    small: "4",
+    capital: "$",
+    code: "Digit4",
+  },
+  {
+    small: "5",
+    capital: "%",
+    code: "Digit5",
+  },
+  {
+    small: "6",
+    capital: "^",
+    code: "Digit6",
+  },
+  {
+    small: "7",
+    capital: "&",
+    code: "Digit7",
+  },
+  {
+    small: "8",
+    capital: "*",
+    code: "Digit8",
+  },
+  {
+    small: "9",
+    capital: "(",
+    code: "Digit9",
+  },
+  {
+    small: "0",
+    capital: ")",
+    code: "Digit0",
+  },
+  {
+    small: "-",
+    capital: "_",
+    code: "Minus",
+  },
+  {
+    small: "=",
+    capital: "+",
+    code: "Equal",
+  },
+  {
+    small: "backspace",
+    capital: "backspace",
+    code: "Backspace",
+  },
+  {
+    small: "tab",
+    capital: "tab",
+    code: "Tab",
+  },
+  {
+    small: "й",
+    capital: "Й",
+    code: "KeyQ",
+  },
+  {
+    small: "ц",
+    capital: "Ц",
+    code: "KeyW",
+  },
+  {
+    small: "у",
+    capital: "У",
+    code: "KeyE",
+  },
+  {
+    small: "к",
+    capital: "К",
+    code: "KeyR",
+  },
+  {
+    small: "е",
+    capital: "Е",
+    code: "KeyT",
+  },
+  {
+    small: "н",
+    capital: "Н",
+    code: "KeyY",
+  },
+  {
+    small: "г",
+    capital: "Г",
+    code: "KeyU",
+  },
+  {
+    small: "ш",
+    capital: "Ш",
+    code: "KeyI",
+  },
+  {
+    small: "щ",
+    capital: "Щ",
+    code: "KeyO",
+  },
+  {
+    small: "з",
+    capital: "З",
+    code: "KeyP",
+  },
+  {
+    small: "х",
+    capital: "Х",
+    code: "BracketLeft",
+  },
+  {
+    small: "ъ",
+    capital: "Ъ",
+    code: "BracketRight",
+  },
+  {
+    small: `\\`,
+    capital: "/",
+    code: "Backslash",
+  },
+  {
+    small: "capslock",
+    capital: "capslock",
+    code: "CapsLock",
+  },
+  {
+    small: "ф",
+    capital: "Ф",
+    code: "KeyA",
+  },
+  {
+    small: "ы",
+    capital: "Ы",
+    code: "KeyS",
+  },
+  {
+    small: "в",
+    capital: "В",
+    code: "KeyD",
+  },
+  {
+    small: "а",
+    capital: "А",
+    code: "KeyF",
+  },
+  {
+    small: "п",
+    capital: "П",
+    code: "KeyG",
+  },
+  {
+    small: "р",
+    capital: "Р",
+    code: "KeyH",
+  },
+  {
+    small: "о",
+    capital: "О",
+    code: "KeyJ",
+  },
+  {
+    small: "л",
+    capital: "Л",
+    code: "KeyK",
+  },
+  {
+    small: "д",
+    capital: "Д",
+    code: "KeyL",
+  },
+  {
+    small: "ж",
+    capital: "Ж",
+    code: "Semicolon",
+  },
+  {
+    small: "э",
+    capital: `Э`,
+    code: "Quote",
+  },
+  {
+    small: "enter",
+    capital: "enter",
+    code: "Enter",
+  },
+  {
+    small: "shift",
+    capital: "shift",
+    code: "ShiftLeft",
+  },
+  {
+    small: "я",
+    capital: "Я",
+    code: "KeyZ",
+  },
+  {
+    small: "ч",
+    capital: "Ч",
+    code: "KeyX",
+  },
+  {
+    small: "с",
+    capital: "С",
+    code: "KeyC",
+  },
+  {
+    small: "м",
+    capital: "М",
+    code: "KeyV",
+  },
+  {
+    small: "и",
+    capital: "И",
+    code: "KeyB",
+  },
+  {
+    small: "т",
+    capital: "Т",
+    code: "KeyN",
+  },
+  {
+    small: "ь",
+    capital: "Ь",
+    code: "KeyM",
+  },
+  {
+    small: "б",
+    capital: "Б",
+    code: "Comma",
+  },
+  {
+    small: "ю",
+    capital: "Ю",
+    code: "Period",
+  },
+  {
+    small: ".",
+    capital: ",",
+    code: "Slash",
+  },
+  {
+    small: "▲",
+    capital: "▲",
+    code: "ArrowUp",
+  },
+  {
+    small: "shift",
+    capital: "shift",
+    code: "ShiftRight",
+  },
+  {
+    small: "ctrl",
+    capital: "ctrl",
+    code: "ControlLeft",
+  },
+  {
+    small: "⌘",
+    capital: "⌘",
+    code: "MetaLeft",
+  },
+  {
+    small: "alt",
+    capital: "alt",
+    code: "AltLeft",
+  },
+  {
+    small: "⌘",
+    capital: "⌘",
+    code: "MetaLeft",
+  },
+  {
+    small: "",
+    capital: "",
+    code: "Space",
+  },
+  {
+    small: "alt",
+    capital: "alt",
+    code: "AltRight",
+  },
+  {
+    small: "◄",
+    capital: "◄",
+    code: "ArrowLeft",
+  },
+  {
+    small: "▼",
+    capital: "▼",
+    code: "ArrowDown",
+  },
+  {
+    small: "►",
+    capital: "►",
+    code: "ArrowRight",
+  },
+  {
+    small: "ctrl",
+    capital: "ctrl",
+    code: "ControlRight",
+  },
+];
---
+++ js/layouts/ru.js
@@ -0,0 +1,322 @@
+export default [
+  {
+    small: "ё",
+    capital: "Ё",
+    code: "Backquote",
+  },
+  {
+    small: "1",
+    capital: "!",
+    code: "Digit1",
+  },
+  {
+    small: "2",
+    capital: `"`,
+    code: "Digit2",
+  },
+  {
+    small: "3",
+    capital: "№",
+    code: "Digit3",
+  },
+  {
+    small: "4",
+    capital: ";",
+    code: "Digit4",
+  },
+  {
+    small: "5",
+    capital: "%",
+    code: "Digit5",
+  },
+  {
+    small: "6",
+    capital: ":",
+    code: "Digit6",
+  },
+  {
+    small: "7",
+    capital: "?",
+    code: "Digit7",
+  },
+  {
+    small: "8",
+    capital: "*",
+    code: "Digit8",
+  },
+  {
+    small: "9",
+    capital: "(",
+    code: "Digit9",
+  },
+  {
+    small: "0",
+    capital: ")",
+    code: "Digit0",
+  },
+  {
+    small: "-",
+    capital: "_",
+    code: "Minus",
+  },
+  {
+    small: "=",
+    capital: "+",
+    code: "Equal",
+  },
+  {
+    small: "backspace",
+    capital: "backspace",
+    code: "Backspace",
+  },
+  {
+    small: "tab",
+    capital: "tab",
+    code: "Tab",
+  },
+  {
+    small: "q",
+    capital: "Q",
+    code: "KeyQ",
+  },
+  {
+    small: "w",
+    capital: "W",
+    code: "KeyW",
+  },
+  {
+    small: "e",
+    capital: "E",
+    code: "KeyE",
+  },
+  {
+    small: "r",
+    capital: "R",
+    code: "KeyR",
+  },
+  {
+    small: "t",
+    capital: "T",
+    code: "KeyT",
+  },
+  {
+    small: "y",
+    capital: "Y",
+    code: "KeyY",
+  },
+  {
+    small: "u",
+    capital: "U",
+    code: "KeyU",
+  },
+  {
+    small: "i",
+    capital: "I",
+    code: "KeyI",
+  },
+  {
+    small: "o",
+    capital: "O",
+    code: "KeyO",
+  },
+  {
+    small: "p",
+    capital: "P",
+    code: "KeyP",
+  },
+  {
+    small: "[",
+    capital: "{",
+    code: "BracketLeft",
+  },
+  {
+    small: "]",
+    capital: "}",
+    code: "BracketRight",
+  },
+  {
+    small: `\\`,
+    capital: "|",
+    code: "Backslash",
+  },
+  {
+    small: "capslock",
+    capital: "capslock",
+    code: "CapsLock",
+  },
+  {
+    small: "a",
+    capital: "A",
+    code: "KeyA",
+  },
+  {
+    small: "s",
+    capital: "S",
+    code: "KeyS",
+  },
+  {
+    small: "d",
+    capital: "D",
+    code: "KeyD",
+  },
+  {
+    small: "f",
+    capital: "F",
+    code: "KeyF",
+  },
+  {
+    small: "g",
+    capital: "G",
+    code: "KeyG",
+  },
+  {
+    small: "h",
+    capital: "H",
+    code: "KeyH",
+  },
+  {
+    small: "j",
+    capital: "J",
+    code: "KeyJ",
+  },
+  {
+    small: "k",
+    capital: "K",
+    code: "KeyK",
+  },
+  {
+    small: "l",
+    capital: "L",
+    code: "KeyL",
+  },
+  {
+    small: ";",
+    capital: ":",
+    code: "Semicolon",
+  },
+  {
+    small: "'",
+    capital: `"`,
+    code: "Quote",
+  },
+  {
+    small: "enter",
+    capital: "enter",
+    code: "Enter",
+  },
+  {
+    small: "shift",
+    capital: "shift",
+    code: "ShiftLeft",
+  },
+  {
+    small: "z",
+    capital: "Z",
+    code: "KeyZ",
+  },
+  {
+    small: "x",
+    capital: "X",
+    code: "KeyX",
+  },
+  {
+    small: "c",
+    capital: "C",
+    code: "KeyC",
+  },
+  {
+    small: "v",
+    capital: "V",
+    code: "KeyV",
+  },
+  {
+    small: "b",
+    capital: "B",
+    code: "KeyB",
+  },
+  {
+    small: "n",
+    capital: "N",
+    code: "KeyN",
+  },
+  {
+    small: "m",
+    capital: "M",
+    code: "KeyM",
+  },
+  {
+    small: ",",
+    capital: "<",
+    code: "Comma",
+  },
+  {
+    small: ".",
+    capital: ">",
+    code: "Period",
+  },
+  {
+    small: "/",
+    capital: "?",
+    code: "Slash",
+  },
+  {
+    small: "▲",
+    capital: "▲",
+    code: "ArrowUp",
+  },
+  {
+    small: "shift",
+    capital: "shift",
+    code: "ShiftRight",
+  },
+  {
+    small: "ctrl",
+    capital: "ctrl",
+    code: "ControlLeft",
+  },
+  {
+    small: "⌘",
+    capital: "⌘",
+    code: "MetaLeft",
+  },
+  {
+    small: "alt",
+    capital: "alt",
+    code: "AltLeft",
+  },
+  {
+    small: "⌘",
+    capital: "⌘",
+    code: "MetaLeft",
+  },
+  {
+    small: "",
+    capital: "",
+    code: "Space",
+  },
+  {
+    small: "alt",
+    capital: "alt",
+    code: "AltRight",
+  },
+  {
+    small: "◄",
+    capital: "◄",
+    code: "ArrowLeft",
+  },
+  {
+    small: "▼",
+    capital: "▼",
+    code: "ArrowDown",
+  },
+  {
+    small: "►",
+    capital: "►",
+    code: "ArrowRight",
+  },
+  {
+    small: "ctrl",
+    capital: "ctrl",
+    code: "ControlRight",
+  },
+];
