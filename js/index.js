6fa63891c12fefd175a2204b17fa1a7055dd0270
MariaKravchenko0 <kravchenkomaria00@gmail.com>
2022-05-07 00:28:18 +0300

    feat: import functions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 "use strict";
+
+import * as main from "./components/main.js";
+import { createKeyboard } from "./components/keyboard.js";
+
+let title = main.createTitle();
+let textarea = main.createTextarea();
+let keyboard = createKeyboard();
+let comment = main.createComment();
+
+document.body.append(title);
+document.body.append(textarea);
+document.body.append(keyboard);
+document.body.append(comment);
