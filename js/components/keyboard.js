103326f946cef5ec8ced73badea935d2d84c2c57
MariaKravchenko0 <kravchenkomaria00@gmail.com>
2022-05-07 00:26:20 +0300

    feat: add draft of creating keyboard layout function

--- js/components/keyboard.js
+++ js/components/keyboard.js
@@ -1,5 +1,110 @@
+import language from "../layouts/lang.js";
+
+const rows = [
+  [
+    "Backquote",
+    "Digit1",
+    "Digit2",
+    "Digit3",
+    "Digit4",
+    "Digit5",
+    "Digit6",
+    "Digit7",
+    "Digit8",
+    "Digit9",
+    "Digit0",
+    "Minus",
+    "Equal",
+    "Backspace",
+  ],
+  [
+    "Tab",
+    "KeyQ",
+    "KeyW",
+    "KeyE",
+    "KeyR",
+    "KeyT",
+    "KeyY",
+    "KeyU",
+    "KeyI",
+    "KeyO",
+    "KeyP",
+    "BracketLeft",
+    "BracketRight",
+    "Backslash",
+  ],
+  [
+    "CapsLock",
+    "KeyA",
+    "KeyS",
+    "KeyD",
+    "KeyF",
+    "KeyG",
+    "KeyH",
+    "KeyJ",
+    "KeyK",
+    "KeyL",
+    "Semicolon",
+    "Quote",
+    "Enter",
+  ],
+  [
+    "ShiftLeft",
+    "KeyZ",
+    "KeyX",
+    "KeyC",
+    "KeyV",
+    "KeyB",
+    "KeyN",
+    "KeyM",
+    "Comma",
+    "Period",
+    "Slash",
+    "ArrowUp",
+    "ShiftRight",
+  ],
+  [
+    "ControlLeft",
+    "MetaLeft",
+    "AltLeft",
+    "Space",
+    "AltRight",
+    "ArrowLeft",
+    "ArrowDown",
+    "ArrowRight",
+    "ControlRight",
+  ],
+];
+
 export function createKeyboard() {
   let keyboard = document.createElement("div");
   keyboard.classList.add("keyboard");
+
+  rows.forEach((row) => {
+    let newRow = createRow(row);
+    keyboard.append(newRow);
+  });
+
   return keyboard;
+}
+
+function createRow(row) {
+  let newRow = document.createElement("div");
+  newRow.classList.add("keyboard__row");
+
+  row.forEach((code) => {
+    let keyObj = language.en.find((key) => key.code === code);
+    let keyEl = createKey(keyObj);
+    newRow.append(keyEl);
+  });
+
+  return newRow;
+}
+
+function createKey(keyObj) {
+  let key = document.createElement("div");
+  key.classList.add("keyboard__key");
+  key.textContent = keyObj.small;
+
+  return key;
 }
